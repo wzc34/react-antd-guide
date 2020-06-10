cff3ef4dcd15d79f993497188dfdc7c57816c0fe
wzc34 <wangzhicheng0304@163.com>
2020-06-10 10:24:30 +0800

    修改antd组件默认中文

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-/**
- * Created by Daguang Li on 11/24/2017.
- */
+/*
+ * @Description: 添加antd组件显示中文
+ * @Autor: wangzc
+ * @Date: 2020-03-09 23:18:18
+ * @LastEditors: wangzc
+ * @LastEditTime: 2020-05-27 17:15:50
+ */ 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import routes from './router/routes'
 import store from "@redux/store/configureStore"
 import registerServiceWorker from './registerServiceWorker'
-import '@fonts/font-awesome.min.css'
 import '@css/style.css'
+import {ConfigProvider} from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
 
 ReactDOM.render((
     <Provider store={store}>
-        {routes}
+        <ConfigProvider locale={zhCN}>
+            {routes}
+        </ConfigProvider>
     </Provider>
 ), document.getElementById("root"));
 
